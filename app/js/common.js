e0ed083250cfc3e91639886ca7a5e25001803325
Kateryna <kateryna@MacBook-Pro-Kateryna.local>
2020-09-10 15:56:58 +0300

    september 10 part 2 changes

--- app/js/common.js
+++ app/js/common.js
@@ -4,8 +4,13 @@ $(function() {
 $('.carousel-people').owlCarousel({
 	loop: true,
 	nav: true,
-	navText: ['', '']
-
+	navText: ['<img src="img/_src/ic-arrow-backward.svg"/>', '<img src="img/_src/ic-arrow-forward.svg"/>'],
+	responsiveClass: true,
+	responsive: {
+		0: { items: 1 },
+		800: { items: 3 },
+		1100: { items: 4 }
+	}
 
 	});
 });
